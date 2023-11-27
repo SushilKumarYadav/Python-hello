78ea2fb7d70c7453e89991724c7f4a1dc7ceb823
Sushil Kumar Yadav <yadab.sushil9762@gmail.com>
2023-11-27 21:26:48 +0530

    Added sorting features

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
 print("Hello")
 
+
 def list_length(list1):
     count = 0
     for item in list1:
         count += 1
     return count
+
+
+def sorting(data):
+    for i in range(len(data)):
+        for j in range(len(data)-i-1):
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+    return data
